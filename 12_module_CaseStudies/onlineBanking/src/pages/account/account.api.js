81d152c087e935a9be6bd278bcf36d82548d06b9
PedroJRGarcia <garciapj85@gmail.com>
2021-09-05 19:49:00 +0200

    commit_pgarcia

--- 12_module_CaseStudies/onlineBanking/src/pages/account/account.api.js
+++ 12_module_CaseStudies/onlineBanking/src/pages/account/account.api.js
@@ -3,13 +3,13 @@ import Axios from 'axios';
 const url = `${process.env.BASE_API_URL}/account`;
 
 export const insertAccount = account => {
-    Axios.post(`${url}/${account.id}`, account).then(({ data }) => data);
+    return Axios.post(`${url}/${account.id}`, account).then(({ data }) => data);
 };
 
 export const getAccount = id => {
-    Axios.get(`${url}/${id}`).then(({ data }) => data);
+    return Axios.get(`${url}/${id}`).then(({ data }) => data);
 };
 
 export const updateAccount = account => {
-    Axios.put(`${url}/${account.id}`, account).then(({ data }) => data);
+    return Axios.put(`${url}/${account.id}`, account).then(({ data }) => data);
 };
