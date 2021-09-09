54cb7aad4613ff0edd2fd5b72913f06c9ff3adea
PedroJRGarcia <garciapj85@gmail.com>
2021-09-09 15:49:09 +0200

    commit_pgarcia

--- 12_module_CaseStudies/onlineBanking/src/pages/movements/movements.api.js
+++ 12_module_CaseStudies/onlineBanking/src/pages/movements/movements.api.js
@@ -1,7 +1,17 @@
-import { Axios } from 'axios';
+import * as Axios from 'axios';
 
 const url = `${process.env.BASE_API_URL}/movements`;
 
-export const getMovementsList = () => {
-    return Axios.get(url).then(({ data }) => data);
+export const getMovementsList = id => {
+    return Axios.get(`${url}/${id}`).then(({ data }) => data);
 }
+
+// export const getMovementsList = () => {
+//     return fetch(url).then(({ data }) => data);
+// }
+
+// export const getMovementsList = () => {
+//     return Axios.get(url).then((res)=>{
+//     return res.data
+//     }).catch(err=>console.log(err))
+// }
--- 12_module_CaseStudies/onlineBanking/src/pages/movements/movements.js
+++ 12_module_CaseStudies/onlineBanking/src/pages/movements/movements.js
@@ -1,9 +1,19 @@
 import { getMovementsList } from './movements.api';
 import { addMovementRows } from './movements.helpers';
 import { mapMovementsListFromApiToVm } from './movements.mappers';
+import { onUpdateField } from '../../common/helpers';
+import { history } from '../../core/router';
 
 getMovementsList().then(movementsList => {
+    console.log({ movementsList });
     const viewModelMovementsList = mapMovementsListFromApiToVm(movementsList);
     addMovementRows(viewModelMovementsList);
-});
 
+    // viewModelMovementsList.forEach(movements => {
+    //     onUpdateField(`${movements.id}`, event => {
+    //         const route = event.target.value;
+    //         history.push(route);
+    //     });
+    // }); 
+    //movement-list
+});
